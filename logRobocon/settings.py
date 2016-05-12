0f418efd2dcfbfe7b0c66a8d607540bcf78f0d03
Macpotty <645092284@qq.com>
2016-05-12 18:53:08 +0800

    部署后的修改

--- logRobocon/settings.py
+++ logRobocon/settings.py
@@ -83,8 +83,6 @@ DATABASES = {
         'PASSWORD': '960627Xc!',
         'HOST': 'localhost',
         'PORT': '3306',
-        'OPTIONS': {
-            'init_command': 'SET storage_engine-INNODB, 'character_set_connection=utf8, 'collation_connection=utf8_unicode_ci
     }
 }
 
